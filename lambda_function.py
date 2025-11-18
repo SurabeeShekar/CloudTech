4ab598d61a5754f86bf02c07f0f7e8aa851f3ab3
SurabeeShekar <143274917+SurabeeShekar@users.noreply.github.com>
2025-11-18 09:12:49 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,10 @@
 import boto3
 
 def lambda_handler(event, context):
-    ec2 = boto3.client("ec2", region_name="ap-south-1")
-    instance_id = "i-00e339b27190269b0"
+    ec2 = boto3.client('ec2', region_name='ap-south-1')
+    instance_id = "i-00e339b27190269b0"   # your EC2 instance
+
     response = ec2.start_instances(InstanceIds=[instance_id])
-    return {"message": "Instance start requested", "response": str(response)}
+    print(response)
+
+    return {"message": "EC2 started"}
